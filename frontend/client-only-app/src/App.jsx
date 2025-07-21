eeec1a8f927f113f99eb737af06451dfb4a83edc
victoria655 <karonjivictoria@gmail.com>
2025-07-21 23:33:44 +0300

    Added routes in App.js so that the Donor component works flawlessly unfortunately Charity routes have failed to be imported during the merge

--- frontend/client-only-app/src/App.jsx
+++ frontend/client-only-app/src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import CharityDescription from './components/Donor/CharityDescription';
+import DonationForm from './components/Donor/DonationForm';
+import DonorDashboard from './components/Donor/DonorDashboard';
+import CharityList from './components/Donor/CharityList';
+import DonationHistory from './components/Donor/DonationHistory';
+import DonorProfileForm from './components/Donor/DonorProfileForm';
+import Updateprofile from './components/Donor/Updateprofile';
 
 function App() {
- 
+  
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<CharityList />} />
+        <Route path="/charities/:id" element={<CharityDescription />} />
+        <Route path="/donate/:id" element={<DonationForm />} />
+        <Route path="/donor/dashboard" element={<DonorDashboard />} />
+        <Route path="/donor/history" element={<DonationHistory />} />
+        <Route path="/donor/profile" element={<DonorProfileForm />} />
+        <Route path="/donor/profile/update" element={<Updateprofile />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;;
+export default App;
